f68a52e0999b86dcee1c564d62a3b4c3eb6d92ca
Adityaraj32 <87028110+Adityaraj32@users.noreply.github.com>
2021-07-07 08:25:03 +0530

    Update Guess_the_number.py

--- Guess_the_number.py
+++ Guess_the_number.py
@@ -1,25 +1,28 @@
 import random
-guess_number2 = random.randint(1, 100)
-guess2 = 0
-max_guess = 9
-game_over = False
-print("This is guess the number game")
-print("Enter number from 1 to 100\n")
+randNum = random.randint(1,100)
+userGuess = None
+gusses = 0
 
-while not game_over:
-    user_input2 = int(input("Enter an number: "))
-    if user_input2 > 100:
-        print("Plese write number less than 100 or 100\n")
-        continue
-    guess2 += 1
-    if guess2 == 9:
-        print("Game over! your life is over")
-        game_over = True
-    elif user_input2 > guess_number2:
-        print(f"Enter Smaller number                                               life:{9 - guess2} \n")
-    elif user_input2 < guess_number2:
-        print(f"Enter Larger number                                               life:{9 - guess2} \n")
-    elif user_input2 == guess_number2:
-        print("Congrats! you win")
-        print(f"You have completed this game in {guess2}")
-        game_over = True
+while (userGuess != randNum):
+    userGuess = int(input("Enter an number: "))
+    if (userGuess == randNum):
+        print("congrats! You guess it right")
+    else:
+        if (userGuess>randNum):
+            print("Enter an smaller number")
+        else:
+            print("Enter an larger number")
+    gusses += 1  
+print(f"Your guess it right in {gusses} gusses")
+
+with open("New_Hiscore.txt","r") as f:
+    Hiscore = int(f.read())
+
+if Hiscore == "":
+    with open("New_Hiscore.txt",'w') as f:
+        f.write(str(gusses))
+
+if gusses<Hiscore:
+    print("You just have Broken the hiscore")
+    with open("New_Hiscore.txt","w") as f:
+        f.write(str(gusses))
